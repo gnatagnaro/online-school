521f8690bcd21cd6e36a0c32978c10893a3985b4
Rustam Khashimov <gnatagnaro@gmail.com>
2023-01-11 12:10:17 +0300

    first_commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <nav class="router-style">
+      <router-link to="/">Домашняя страница</router-link> |
+      <router-link to="/about">Оставьте заявку</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+}
+
+.router-style {
+  text-align: center;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #3172b9;
+}
+</style>
